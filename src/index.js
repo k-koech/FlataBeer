bd9dccd2bfd00a0df796c73a0a920457217c7c2d
k-koech <koechkelvin97@gmail.com>
2022-11-02 21:56:14 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-const url = 'http://localhost:3000/beers/'
+const url = 'https://flatabeer001.herokuapp.com/beers/'
 let parentEl = document.getElementById('beer-list')
 
 document.addEventListener('DOMContentLoaded', () => {
